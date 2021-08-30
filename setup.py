69be551eae951d24fa8b409b070f338f1b784593
Vitche Research Team <developer@vitche.com>
2021-08-30 19:41:43 +0300

    1. Initial check-in.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
    name='hysh_script',
-   version='1.1',
+   version='1.2',
    description='HyperShell script',
    author='Vitche Research Team',
    author_email='developer@vitche.com',
-   py_modules=['hysh.script'],
+   py_modules=['hysh.script.template', 'hysh.script.cluster'],
    install_requires=[]
 )
